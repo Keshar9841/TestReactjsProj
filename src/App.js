1ad44af25b8382922726afdb4c1537ccc5a55385
Krishna Bikram Thapa <kbikram0082@gmail.com>
2018-10-03 20:20:58 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+      // <div className="App">
+
+      //   <header className="App-header">
+      //   <nav className="navbar  navbar-expand-lg navbar-light bg-light">
+      //       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      //         <span className="navbar-toggler-icon"></span>
+      //       </button>
+      //       <a className="navbar-brand" href="#">Navbar</a>
+
+      //       <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+      //         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      //           <li className="nav-item active">
+      //             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      //           </li>
+      //           <li className="nav-item">
+      //             <a className="nav-link" href="#">Link</a>
+      //           </li>
+      //           <li className="nav-item">
+      //             <a className="nav-link disabled" href="#">Disabled</a>
+      //           </li>
+      //         </ul>
+      //         <form className="form-inline my-2 my-lg-0">
+      //           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      //           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      //         </form>
+      //       </div>
+      //     </nav>
+
+
+      //   </header>
+
+      //   <div className="row">
+      //     <div className="col-sm-6">
+      //       <div className="card">
+      //         <div className="card-body">
+      //           <h5 className="card-title">Special title treatment</h5>
+      //           <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      //           <a href="#" className="btn btn-primary">Go somewhere</a>
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <div className="col-sm-6">
+      //       <div className="card">
+      //         <div className="card-body">
+      //           <h5 className="card-title">Special title treatment</h5>
+      //           <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      //           <a href="#" className="btn btn-primary">Go somewhere</a>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      //   <form>
+      //     <div className="form-group">
+      //       <label htmlFor="exampleInputEmail1">Email address</label>
+      //       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+      //       <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      //     </div>
+      //     <div className="form-group">
+      //       <label htmlFor="exampleInputPassword1">Password</label>
+      //       <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      //     </div>
+      //     <div className="form-check">
+      //       <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+      //       <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+      //     </div>
+      //     <div className="input-group mb-3">
+      //       <div className="input-group-prepend">
+      //         <span className="input-group-text" id="basic-addon1">@</span>
+      //       </div>
+      //       <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+      //     </div>
+
+      //     <div className="input-group mb-3">
+      //       <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+      //       <div className="input-group-append">
+      //         <span className="input-group-text" id="basic-addon2">@example.com</span>
+      //       </div>
+      //     </div>
+
+      //     <label htmlFor="basic-url">Your vanity URL</label>
+      //     <div className="input-group mb-3">
+      //       <div className="input-group-prepend">
+      //         <span className="input-group-text" id="basic-addon3">https://example.com/users/</span>
+      //       </div>
+      //       <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
+      //     </div>
+
+      //     <div className="input-group mb-3">
+      //       <div className="input-group-prepend">
+      //         <span className="input-group-text">$</span>
+      //       </div>
+      //       <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
+      //       <div className="input-group-append">
+      //         <span className="input-group-text">.00</span>
+      //       </div>
+      //     </div>
+
+      //     <div className="input-group">
+      //       <div className="input-group-prepend">
+      //         <span className="input-group-text">With textarea</span>
+      //       </div>
+      //       <textarea className="form-control" aria-label="With textarea"></textarea>
+      //     </div>
+      //     <button type="submit" className="btn btn-primary">Submit</button>
+      //   </form>
+      // </div>
+
+    );
+  }
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// const name = 'Josh Perez';
+// const element = <div><h1>Hello, {name}</h1> <div><App/></div></div>;
+
+// function formatName(user) {
+//     return user.firstName + ' ' + user.lastName;
+//   }
+
+//   const user = {
+//     firstName: 'Harper',
+//     lastName: 'Perez',
+//   };
+
+//   const element = <h1>Hello, {formatName(user)}!</h1>;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+  
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+       <NumberList/>
+          <div className="UserInfo-name">
+            {props.author.name}
+          </div>
+        </div>
+        <div className="Comment-text">{props.text}</div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  }
+  
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: [{
+      name: 'Hello Kitty',
+      avatarUrl: ['http://placekitten.com/g/64/64'],
+    },{
+        name: 'Hello Kitty1',
+        avatarUrl: ['http://placekitten.com/g/64/64'],
+      },{
+        name: 'Hello Kitty2',
+        avatarUrl: ['http://placekitten.com/g/64/64'],
+      },{
+        name: 'Hello Kitty3',
+        avatarUrl: ['http://placekitten.com/g/64/64'],
+      }],
+  };
+  function Imagelist(props){
+      console.log(props);
+    return <img
+    className="Avatar"
+    src={props.value.avatarUrl}
+    alt={props.value.name}
+  />
+  }
+  function NumberList() {
+    const list= comment.author.map((aut,index)=>   <div key={index}><Imagelist value={aut}/> </div>);
+
+    return list;
+  }
+  
+  ReactDOM.render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
+    document.getElementById('root')
+  );
+  
+
+
